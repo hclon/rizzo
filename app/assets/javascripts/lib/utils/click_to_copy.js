fef3d50c4861f9f371c14dcd696db1cfa9821a7f
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2016-09-28 16:18:07 +0200

    Display copying feedback

--- app/assets/javascripts/lib/utils/click_to_copy.js
+++ app/assets/javascripts/lib/utils/click_to_copy.js
@@ -1,8 +1,37 @@
 define(function() {
   "use strict";
 
-  function copyToClipboard(colour) {
-    var target = document.createElement("textarea");
+  function fadeOutElement(element) {
+    window.elem = element.parentElement;
+    var fadeEffect = setInterval(function() {
+      if (element.style.opacity > 0) {
+        element.style.opacity -= 0.1;
+      } else {
+        element.textContent = "";
+        element.parentElement.style.height = "24px";
+        element.parentElement.style.paddingBottom = "25px";
+        clearInterval(fadeEffect);
+      }
+    }, 100);
+  }
+
+  function flashFeedback($colourTile, colour) {
+    window.tile = $colourTile;
+    var feedbackMessage = document.createElement("div");
+    feedbackMessage.style.marginTop = "-8px";
+    feedbackMessage.style.color = colour;
+    feedbackMessage.style.filter = "invert(100%)";
+    feedbackMessage.style.opacity = "1";
+    feedbackMessage.textContent = "Copied!";
+
+    $colourTile.style.paddingBottom = "9px";
+    $colourTile.appendChild(feedbackMessage);
+    fadeOutElement(feedbackMessage);
+  }
+
+  function copyToClipboard($colourTile) {
+    var colour = $colourTile.textContent,
+        target = document.createElement("textarea");
     target.style.position = "absolute";
     target.style.left = "-9999px";
     target.style.top = "0";
@@ -19,11 +48,12 @@ define(function() {
     window.scrollTo(currentX, currentY);
     target.textContent = "";
     target.outerHTML = "";
+    flashFeedback($colourTile, colour);
   }
 
-  function ClickToCopy($target) {
-    $target.addEventListener("click", function() {
-      copyToClipboard($target.textContent);
+  function ClickToCopy($colourTile) {
+    $colourTile.addEventListener("click", function() {
+      copyToClipboard($colourTile);
     });
   }
 
